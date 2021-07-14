6e5a9979f004ff51dc410253913a026ee0c1ee02
achmadky <achmad.dhikrillah@gmail.com>
2021-07-14 23:06:03 +0700

    add quote

--- components/Header.js
+++ components/Header.js
@@ -11,9 +11,9 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Welcome to my site</h1>
+                <p>"Fake it till you make it"<br />
+                -Amy Cuddy</p>
             </div>
         </div>
         <nav>
